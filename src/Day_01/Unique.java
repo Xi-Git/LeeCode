c6e74e6d3f042ef1b2538e454ac7fa56983aa9c3
Xi <Xi@example.com>
2020-11-13 19:28:27 +0800

    第一天的提交内容

---
+++ src/Day_01/Unique.java
@@ -0,0 +1,58 @@
+package Day_01;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+/***
+ * https://leetcode-cn.com/problems/is-unique-lcci/
+ * 实现一个算法，确定一个字符串 s 的所有字符是否全都不同。
+ * @author MyPC
+ *
+ */
+
+public class Unique {
+	public static void main(String[] args) {
+		String s = new String();
+		Scanner sc = new Scanner(System.in);
+		s = sc.nextLine();
+		
+		System.out.println(Solution.isUnique(s));
+
+	}
+
+}
+class Solution {
+//	解法1：利用String的函数
+    public static boolean isUnique(String astr) {
+		for(int i=0;i<astr.length();i++){
+			if(astr.indexOf(astr.charAt(i))!= astr.lastIndexOf(astr.charAt(i))){
+				return false;
+			}
+		}
+		return true;
+    }
+    
+//    解法二:遍历String，每次遍历使用indexof(c,index)检查遍历的后面是否还有该字符
+    public boolean isUnique_3(String astr) {
+        for (int i = 0; i < astr.length(); i++) {
+            char c = astr.charAt(i);
+            //查看后面是否有当前字符
+            if (astr.indexOf(c, i + 1) != -1)
+                return false;
+        }
+        return true;
+    }
+
+    
+//    解法三:利用hashSet的不可重复
+    public static boolean isUnique_2(String astr) {
+    	HashSet<Character> es = new HashSet<Character>();
+    	for(char c : astr.toCharArray()){
+            if(!es.add(c)) {
+                return false;
+            }
+        }
+		return true;
+    }
+    
+}
---
+++ src/Day_02/CheckPermutation.java
@@ -0,0 +1,60 @@
+package Day_02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class CheckPermutation {
+
+	/**
+	 * https://leetcode-cn.com/problems/check-permutation-lcci/ 给定两个字符串 s1 和
+	 * s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.nextLine();
+		String s2 = sc.nextLine();
+		System.out.println(CheckPermutation.judge(s1, s2));
+
+	}
+
+	// 解法一: 通过比较排序后的数组的每个元素是否相等   使用Arrays自带的方法进行处理
+	public static boolean judge(String s1, String s2) {
+		if (s1.length() != s2.length()) {
+			return false;
+		} else {
+			char[] x1 = s1.toCharArray();
+			char[] x2 = s2.toCharArray();
+			Arrays.sort(x1);
+			Arrays.sort(x2);
+			for (int i = 0; i < x1.length; i++) {
+				if (x1[i] != x2[i]) {
+					return false;
+				}
+			}
+			// Arrays.equals(x1, x2); 该方法重写过，不是比较地址，会比较数组中的每个元素是否相等
+		}
+		return true;
+	}
+	// 解法二: 使用int数组进行标记
+	public boolean judge2(String s1, String s2) {
+		if (s1.length() != s2.length())
+			return false;
+		int[] nums = new int[26];
+		int len = s1.length();
+		for (int i = 0; i < len; i++) {
+			nums[s1.charAt(i) - 97]++;				//核心代码：分别检测两个数组元素，并使用int数组+1进行标记
+			nums[s2.charAt(i) - 97]--;				//		     一个数组检测到就进行加，另一个数组检测到就-1，最
+		}											//		     终如果两个数组一致就会返回一个全为零的int数组
+		for (int num : nums) {
+			if (num != 0)
+				return false;
+		}
+		return true;
+	}
+
+}
