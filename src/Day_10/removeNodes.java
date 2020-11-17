7fabe78716e5326b13afe6c7ec0c98218c84404d
GitMars <Xi@example.com>
2020-11-17 12:36:21 +0800

    这是第五天的第二题

---
+++ src/Day_10/removeNodes.java
@@ -0,0 +1,34 @@
+package Day_10;
+import java.util.HashSet;
+// Definition for singly-linked list.
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+public class removeNodes {
+	/***
+	 * https://leetcode-cn.com/problems/remove-duplicate-node-lcci/
+	 * 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+	 * @author MyPC
+	 *
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}	
+	public ListNode removeDuplicateNodes(ListNode head) {
+		HashSet<Integer> hashSet = new HashSet<Integer>();
+		ListNode cur = head ;
+		while( cur != null && cur.next != null ){
+			hashSet.add(cur.val);
+			if(hashSet.contains(cur.next.val)){
+				cur.next = cur.next.next;
+			} else {
+				cur = cur.next;
+			}
+		}
+		return head;		
+	}
+
+}
