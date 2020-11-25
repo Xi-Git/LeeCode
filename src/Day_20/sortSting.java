fc0ddbe7ff9520b9e8a5c16affb3bc6e1b8509f1
GitMars <Xi@example.com>
2020-11-25 13:37:44 +0800

    更新Day_21的第二种解法

--- src/Day_20/sortSting.java
+++ src/Day_20/sortSting.java
@@ -1,12 +1,9 @@
 package Day_20;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * 
@@ -28,6 +25,7 @@ import java.util.TreeSet;
 
 public class sortSting {
 
+	//解法一:桶计数
 	public String SortString(String s) {
     	StringBuilder ret =new StringBuilder();
     	char[] strchar = s.toCharArray();
@@ -55,5 +53,52 @@ public class sortSting {
     	return ret.toString();
     }
     
+	//解法二:通过map的特性将String的所有元素统计后加入，再通过map获取键值对存入Map.Entry
+	 public String sortString2(String s) {
+	        //将字符串按照各字符大小装入TreeMap中，key为字符，value为出现次数
+	        TreeMap<Character, Integer> params = new TreeMap<>(new Comparator<Object>() {
+	        	@Override
+	        	public int compare(Object o1, Object o2) {
+	        		// TODO Auto-generated method stub
+	        		return (Integer.parseInt((String)o1))-(Integer.parseInt((String)o2));
+	        	}
+			});
+	        for (char item: s.toCharArray()) {
+	            if(params.containsKey(item)){
+	                params.put(item, params.get(item)+1);
+	            }else {
+	                params.put(item, 1);
+	            }
+	        }
+
+	        StringBuilder result = new StringBuilder();
+	        Boolean desc = true;
+	        //由题意知结果字符串为先从小到大筛选各字符排列，然后从大到小筛选排列，然后又从小到大筛选排列，直至全部筛选完
+	        //故此可利用有序treeMap的higherEntry和lowerEntry方法进行筛选获取
+	        Map.Entry<Character, Integer> entry = params.firstEntry();
+	        Map.Entry<Character, Integer> temp = entry;
+	        while (params.size() > 0){
+	            if(entry != null){
+	                result.append(entry.getKey());
+	                if(entry.getValue() > 1){
+	                    params.put(entry.getKey(), entry.getValue()-1);
+	                }else {
+	                    params.remove(entry.getKey());
+	                }
+	            }else {
+	                desc = !desc;
+	            }
+	            if(desc){
+	                entry = (entry != null ?params.higherEntry(entry.getKey()) : params.firstEntry());
+	            }else {
+	                entry = (entry != null ?params.lowerEntry(entry.getKey()) : params.lastEntry());
+	            }
+
+	        }
+	        return result.toString();
+	    }
+	 
+	 
+	
 
 }
